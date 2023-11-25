b4124652a05456e88e4a986e17081f121f57380e
SHerlihy <steven_herlihy@yahoo.com>
2023-11-25 15:51:32 +0000

    proxy remainning auth calls

--- env_vars/env_vars.go
+++ env_vars/env_vars.go
@@ -14,6 +14,7 @@ func ProdEnv(){
 func DevEnv(){
     authServiceURL := "http://localhost:5001/api/v1"
 
+    os.Setenv("CLIENT_URL", "http://localhost:5173")
     os.Setenv("AUTH_SERVICE_URL", authServiceURL)
-    os.Setenv("ALLOWED_ORIGINS", "http://localhost:5173, http://localhost:5001, http://localhost:5000")
+    os.Setenv("ALLOWED_ORIGINS", "http://localhost:5173")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,10 +1,7 @@
 package routes
 
 import (
-	"bytes"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
@@ -12,61 +9,32 @@ import (
 )
 
 func Setup(router fiber.Router) {
-//	router.Get("/user/logout")
-//	router.Get("/user/auth-status")
-
-    //authServiceURL := os.Getenv("AUTH_SERVICE_URL")
-    //reqURL := fmt.Sprintf("%s%s", authServiceURL, "/user/signup")
-    //router.Post("/user/signup", preAuthCall, proxy.DomainForward(reqURL,"http://localhost:5000" ))
-    //router.Post("/user/signup", preAuthCall, proxy.Forward(reqURL))
-    router.Post("/user/signup", preAuthCall, proxyDo)
-    //router.Post("/user/signup", preAuthCall, proxyAuthService)
-//	router.Post("/user/login")
+    router.Get("/user/auth-status", func(c *fiber.Ctx)error{
+        return authProxy(c,"/user/auth-status")
+    })
+    router.Get("/user/logout", func(c *fiber.Ctx)error{
+        return authProxy(c,"/user/logout")
+    })
+    
+    router.Post("/user/signup", func(c *fiber.Ctx)error{
+        return authProxy(c,"/user/signup")
+    })
+    router.Post("/user/login", func(c *fiber.Ctx)error{
+        return authProxy(c,"/user/login")
+    })
 }
 
-func proxyDo(c *fiber.Ctx)error{
+func authProxy(c *fiber.Ctx, endpoint string) error {
+    clientURL := os.Getenv("CLIENT_URL")
     authServiceURL := os.Getenv("AUTH_SERVICE_URL")
-    reqURL := fmt.Sprintf("%s%s", authServiceURL, "/user/signup")
+    reqURL := fmt.Sprintf("%s%s", authServiceURL, endpoint)
 
     if err := proxy.Do(c,reqURL); err!=nil{
         return err
     }
 
-    c.Response().Header.Set("Access-Control-Allow-Origin", "http://localhost:5173")
+    c.Response().Header.Set("Access-Control-Allow-Origin", clientURL)
     c.Response().Header.Set("Access-Control-Allow-Credentials", "true")
 
     return nil
-}
-
-func proxyAuthService(c *fiber.Ctx) error {
-    authServiceURL := os.Getenv("AUTH_SERVICE_URL")
-    reqURL := fmt.Sprintf("%s%s", authServiceURL, "/api/v1/user/signup")
-    var buff bytes.Buffer
-
-    buff.Write(c.BodyRaw())
-
-    fmt.Println(os.Stdout, fmt.Sprint("pre post req"))
-    resp, err := http.Post(reqURL, "application/json", &buff)
-    if err != nil {
-    	// handle error
-        fmt.Println(os.Stdout, fmt.Sprint("Errror"))
-    }
-    defer resp.Body.Close()
-    body, err := io.ReadAll(resp.Body)
-
-    fmt.Println(os.Stdout, fmt.Sprint("post body %v", body))
-
-    respCookies := resp.Cookies()
-
-    for _, respCookie := range respCookies {
-        c.CookieParser(respCookie)
-    }
-
-    return c.JSON(body)
-}
-
-func preAuthCall(c *fiber.Ctx)error{
-    fmt.Println(os.Stdout, fmt.Sprint("Pre call response"))
-
-    return c.Next()
 }
